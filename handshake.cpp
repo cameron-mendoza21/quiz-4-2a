2b7675565f997132ac002835e73d5dffcca81830
Cameron Mendoza <mendoza.cameron21@gmail.com>
2022-10-27 21:41:26 -0500

    code works

--- handshake.cpp
+++ handshake.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 // include additional necessary headers
+#include <thread>
+#include <unistd.h>
+#include <mutex>
+#include <string>
 
-void query(/*add necessary parameters*/) {
+using namespace std;
+
+void query(int num/*add necessary parameters*/) {
     // Should print: the print number (starting from 0), "SYN", and the three dots "..."
+    printf("[%d] SYN ... ", num);
 }
 
 void response(/*add necessary parameters*/) {
     // Should print "ACK"
+    printf("ACK\n");
 }
 
 int main(int argc, char** argv) {
@@ -14,7 +22,8 @@ int main(int argc, char** argv) {
         printf("Usage: ./handshake <count>\n");
         exit(1);
     }
-
+    int count = 0;
+    
     /**
      * Steps to follow:
      * 1. Get the "count" from cmd args
@@ -23,6 +32,30 @@ int main(int argc, char** argv) {
      * 4. Provide the threads with necessary args
      * 5. Update the "query" and "response" functions to synchronize the output
     */
-   
+
+    // getopt to read count
+    int opt;
+    while ((opt = getopt(argc, argv, "c:")) != -1) {
+        switch (opt) {
+            case 'c':
+                count = atoi(optarg);
+                break;
+        }
+    }
+
+    mutex m;
+
+    for (int i = 0; i < count ; ++i)
+    {
+        m.lock();
+        thread first(query, i);
+        m.unlock();
+        thread second(response);
+    
+        first.join();
+        second.join();
+        
+    }
+    
     return 0;
 }
